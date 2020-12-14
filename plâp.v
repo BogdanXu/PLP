5e414f78954ca2053c0577258459225eec46422e
Iovite Bogdan <iovite_bogdan@yahoo.com>
2020-12-14 14:51:58 +0200

    Update plâp.v

--- plâp.v
+++ plâp.v
@@ -7,9 +7,6 @@ Require Vectors.Fin.
 Import EqNotations.
 Local Open Scope nat_scope.
 
-Inductive ErrorBool :=
-  | error_bool : ErrorBool
-  | boolean : bool -> ErrorBool.
 
 Inductive ErrorNat :=
   | error_nat : ErrorNat
@@ -19,16 +16,19 @@ Inductive ErrorString :=
   | error_string : ErrorString
   | str : string -> ErrorString.
 
-Coercion boolean: bool >-> ErrorBool.
+Inductive ErrorBool :=
+  | error_bool : ErrorBool
+  | boolean : bool -> ErrorBool.
+
+
 Coercion num: nat >-> ErrorNat.
 Coercion str: string >-> ErrorString.
+Coercion boolean: bool >-> ErrorBool.
 
-Check ErrorBool.
+(*Check ErrorBool.
 Check ErrorNat.
 Check ErrorString.
-Check bool.
-
-
+Check bool.*)
 
 Inductive NatExp:=
   | apls: NatExp -> NatExp -> NatExp
@@ -40,9 +40,11 @@ Inductive NatExp:=
   | astr: string -> NatExp.
 
 Inductive StrExp :=
-  | strconc : StrExp -> StrExp -> StrExp
-  | struppr : StrExp -> StrExp
-  | strlowr : StrExp -> StrExp
+  | strcat : StrExp -> StrExp -> StrExp
+  | strupper : StrExp -> StrExp
+  | strlower : StrExp -> StrExp
+  | strset : StrExp -> StrExp -> StrExp
+  | strnum : ErrorString -> StrExp
   | strvar: string -> StrExp.
 
 Inductive BoolExp :=
@@ -57,4 +59,20 @@ Inductive BoolExp :=
   | bstrcmp : StrExp -> StrExp -> BoolExp
   | bnum: ErrorBool -> BoolExp
   | bstr: string -> BoolExp.
+
+Inductive Stmt :=
+  | nat_decl: string -> NatExp -> Stmt 
+  | bool_decl: string -> BoolExp -> Stmt 
+  | str_decl: string -> StrExp -> Stmt 
+
+  | nat_assign : string -> NatExp -> Stmt
+  | bool_assign : string -> BoolExp -> Stmt 
+  | str_assign : string -> StrExp -> Stmt 
+
+  | while : BoolExp -> Stmt -> Stmt
+  | ifthenelse : BoolExp -> Stmt -> Stmt -> Stmt
+  | ifthen : BoolExp -> Stmt -> Stmt
+  | sequence : Stmt -> Stmt -> Stmt.
+
+
 

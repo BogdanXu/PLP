cdea99aa2d9478d5df93c9834409e45005d27108
Iovite Bogdan <iovite_bogdan@yahoo.com>
2020-12-14 13:48:00 +0200

    Update plâp.v

--- plâp.v
+++ plâp.v
@@ -2,6 +2,10 @@ Require Import Strings.String.
 Local Open Scope string_scope.
 Local Open Scope list_scope.
 Scheme Equality for string.
+Require Import Arith_base.
+Require Vectors.Fin.
+Import EqNotations.
+Local Open Scope nat_scope.
 
 Inductive ErrorNat :=
   | error_nat : ErrorNat
@@ -15,6 +19,11 @@ Inductive ErrorString :=
   | error_string : ErrorString
   | str : string -> ErrorString.
 
+Inductive ErrorVector :=
+  | error_vector : ErrorVector
+  | vctr : vector -> ErrorVector.
+
 Coercion num: nat >-> ErrorNat.
 Coercion boolean: bool >-> ErrorBool.
 Coercion str: string >-> ErrorString.
+Coercion vctr : 

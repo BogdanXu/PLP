fc730dc4aeb70f2f1537fa1932d51e38bbc3561f
Iovite Bogdan <iovite_bogdan@yahoo.com>
2020-12-14 17:38:29 +0200

    Update plâp.v

--- plâp.v
+++ plâp.v
@@ -40,11 +40,11 @@ Inductive NatExp:=
   | natstr: string -> NatExp.
 
 
-Notation "A !+ B" := (natpls A B)(at level 20, left associativity).
-Notation "A !* B" := (natmul A B)(at level 19, left associativity).
-Notation "A !- B" := (natsub A B)(at level 20, left associativity).
-Notation "A !/ B" := (natdiv A B)(at level 19, left associativity).
-Notation "A !% B" := (natmod A B)(at level 19, left associativity).
+Notation "A .+ B" := (natpls A B)(at level 20, left associativity).
+Notation "A .* B" := (natmul A B)(at level 19, left associativity).
+Notation "A .- B" := (natsub A B)(at level 20, left associativity).
+Notation "A ./ B" := (natdiv A B)(at level 19, left associativity).
+Notation "A .% B" := (natmod A B)(at level 19, left associativity).
 
 Inductive StrExp :=
   | strcat : StrExp -> StrExp -> StrExp
@@ -53,10 +53,10 @@ Inductive StrExp :=
   | strset : StrExp -> StrExp -> StrExp
   | strvar : string -> StrExp.
 
-Notation "S !strcat S'" := (strcat S S')(at level 40).
-Notation "!upper S" := (strupper S)(at level 40).
-Notation "!lower S" := (strlower S)(at level 40).
-Notation "!strset S S'" := (strset S S')(at level 40).
+Notation "S .strcat S'" := (strcat S S')(at level 40).
+Notation ".upper S" := (strupper S)(at level 40).
+Notation ".lower S" := (strlower S)(at level 40).
+Notation ".strset S S'" := (strset S S')(at level 40).
 
 Inductive BoolExp :=
   | btrue
@@ -71,12 +71,12 @@ Inductive BoolExp :=
   | bnum: ErrorBool -> BoolExp
   | bstr: string -> BoolExp.
 
-Notation "A !< B" := (blessthan A B) (at level 70).
-Notation "A !> B" := (bgreaterthan A B) (at level 70).
-Notation "!! A" := (bnot A)(at level 71, left associativity).
-Notation "A !&& B" := (band A B)(at level 72, left associativity).
-Notation "A !|| B" := (bor A B)(at level 73, left associativity).
-Notation "A !strcmp B" := (bstrcmp A B)(at level 73, left associativity).
+Notation "A .< B" := (blessthan A B) (at level 70).
+Notation "A .> B" := (bgreaterthan A B) (at level 70).
+Notation ".! A" := (bnot A)(at level 71, left associativity).
+Notation "A .&& B" := (band A B)(at level 72, left associativity).
+Notation "A .|| B" := (bor A B)(at level 73, left associativity).
+Notation "A .strcmp B" := (bstrcmp A B)(at level 73, left associativity).
 
 Inductive Stmt :=
   | declare_nat: string -> NatExp -> Stmt 
@@ -92,18 +92,18 @@ Inductive Stmt :=
   | ifelse : BoolExp -> Stmt -> Stmt -> Stmt
   | sequence : Stmt -> Stmt -> Stmt.
 
-Notation "X !n= A" := (nat_assign X A)(at level 75).
-Notation "X !s= A" := (str_assign X A)(at level 75).
-Notation "X !b= A" := (bool_assign X A)(at level 75).
+Notation "X .n= A" := (nat_assign X A)(at level 75).
+Notation "X .s= A" := (str_assign X A)(at level 75).
+Notation "X .b= A" := (bool_assign X A)(at level 75).
 
-Notation "!Nat X ::= A" := (declare_nat X A)(at level 75).
-Notation "!Str X ::= A" := (declare_string X A)(at level 75).
-Notation "!Bool X ::= A" := (declare_bool X A)(at level 75).
+Notation ".Nat X ::= A" := (declare_nat X A)(at level 75).
+Notation ".Str X ::= A" := (declare_string X A)(at level 75).
+Notation ".Bool X ::= A" := (declare_bool X A)(at level 75).
 
 Notation "S1 , S2" := (sequence S1 S2) (at level 78).
-Notation "!whiled ( A )==>{ S } " := (while A S)(at level 80). 
-Notation "!ford ( A ; B ; C )==>{ S }" := (A , while B ( S , C )) (at level 80).
-(*Notation "!ifthen ( A ) { S } " := (ifthen A S) (at level 10). (*nu mergea fara lvl foarte mic wat*)*)
+Notation ".whiled ( A )==>{ S } " := (while A S)(at level 80). 
+Notation ".ford ( A ; B ; C )==>{ S }" := (A , while B ( S , C )) (at level 80).
+(*Notation ".ifthen ( A ==>{ S } " := (ifthen A S) (at level 10). (*nu mergea fara lvl foarte mic wat*)*)
 
 
 Reserved Notation "A =[ S ]=> N" (at level 90).
@@ -118,36 +118,36 @@ Coercion bstr: string >-> BoolExp.
 Coercion strvar: string >-> StrExp.
 
 Definition plsmergi1 :=
-  !Nat "i" ::= 0,
-  !Nat "j" ::= 1,
-  !Nat "k" ::= 0, 
-  "k" !n= "i" !+ "j", 
-  "j" !n= "j" !- "i",
-  "i" !n= "j" !* "i", 
-  "j" !n= 2 !+ 1,
-  "j" !n= "i" !/ "j",   "j" !n= "i" !% "j".
+  .Nat "i" ::= 0,
+  .Nat "j" ::= 1,
+  .Nat "k" ::= 0, 
+  "k" .n= "i" .+ "j", 
+  "j" .n= "j" .- "i",
+  "i" .n= "j" .* "i", 
+  "j" .n= 2 .+ 1,
+  "j" .n= "i" ./ "j",   "j" .n= "i" .% "j".
 
 Check plsmergi1.
 
 
 Definition plsmergi2 :=
-  "b1" !b= true,
-  !Bool "b1" ::= true,
-  !Bool "b2" ::= false,
-  !ford ("i" !n= 1 ; "i" !< 5 ; "i" !n= "i" !+ 1)==>{"s1" !s= "s1" !strcat "hello"}.
+  "b1" .b= true,
+  .Bool "b1" ::= true,
+  .Bool "b2" ::= false,
+  .ford ("i" .n= 1 ; "i" .< 5 ; "i" .n= "i".+1)==>{"s1" .s= "s1" .strcat "hello"}.
 
 Check plsmergi2.
   
 Definition plsmergi3 :=
-  !whiled ("k" !> 10)==>{"b1" !b= false}.
+  .whiled ("k" .> 10)==>{"b1" .b= false}.
 
 Check plsmergi3.
   
 Definition plsmergi4 :=
-  !ford ("i" !n= 1 ; "i" !< 5 ; "i" !n= "i" !+ 1)==>{"b1" !b= false},
-  !Bool "b1" ::= true,
-  !Bool "b2" ::= false,
-  !whiled ("k" !> 10)==>{"b1" !b= false}. 
+  .ford ("i" .n= 1 ; "i" .< 5 ; "i" .n= "i" .+ 1)==>{"b1" .b= false},
+  .Bool "b1" ::= true,
+  .Bool "b2" ::= false,
+  .whiled ("k" .> 10)==>{"b1" .b= false}. 
   
 
 
